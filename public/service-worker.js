3ea514bfbd3b3f38f2295768778720e30cb126b4
holinei <holiney2406@gmail.com>
2025-02-07 20:40:29 +0100

    Update 0.13

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,52 @@
-const CACHE_NAME = "todo-cache-v1";
-const urlsToCache = ["/", "/index.html", "/main.css", "/main.js"];
+// const CACHE_NAME = "todo-cache-v1";
+// const urlsToCache = ["/", "/index.html", "/main.css", "/main.js"];
+
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       console.log("Кешування ресурсів");
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
+// self.addEventListener("activate", (event) => {
+//   const cacheWhitelist = [CACHE_NAME];
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) =>
+//       Promise.all(
+//         cacheNames.map((cacheName) => {
+//           if (!cacheWhitelist.includes(cacheName)) {
+//             console.log("Видалення старого кешу:", cacheName);
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       )
+//     )
+//   );
+// });
+const CACHE_NAME = "worktime-tracker-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/static/js/bundle.js",
+  "/static/css/main.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Кешування ресурсів");
-      return cache.addAll(urlsToCache);
+      return cache.addAll(ASSETS);
     })
   );
 });
@@ -15,21 +56,5 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            console.log("Видалення старого кешу:", cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      )
-    )
   );
 });
