3f0aed3d042a2f454e23f4dc9e340e25ed5a9fcd
holinei <holiney2406@gmail.com>
2025-02-07 23:31:21 +0100

    Update 0.184

--- public/service-worker.js
+++ public/service-worker.js
@@ -23,6 +23,20 @@ self.addEventListener("install", (event) => {
   );
 });
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Видаляємо старі кеші
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
